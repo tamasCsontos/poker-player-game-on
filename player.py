79dbd653c470289a91459330bed4c4957434c182
Csonti <csontostamas1991@gmail.com>
2018-05-14 13:34:16 +0200

    sadsa

--- player.py
+++ player.py
@@ -17,7 +17,6 @@ class Player:
     def betRequest(self, game_state):
         own_player = get_own_player(game_state['players'])
         hole_cards = own_player['hole_cards']
-        print "asdsadasd: " + own_player
 
         if card.are_card_ranks_equal(hole_cards) or card.is_card_with_rank(hole_cards, "A"):
             return get_all_in_amount(own_player)
