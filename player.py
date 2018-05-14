34f2447c888838257a1d85e97cde7f7c6a660d07
Balogh David <balogh.david1015@gmail.com>
2018-05-14 14:57:18 +0200

    pgf test

--- player.py
+++ player.py
@@ -17,7 +17,7 @@ class Player:
     def betRequest(self, game_state):
         own_player = get_own_player(game_state['players'])
         hole_cards = own_player['hole_cards']
-        if int(game_state['round']) < 3:
+        if int(game_state['round']) == 1:
             if card.are_card_ranks_equal(hole_cards):
                     if card.is_card_under_ten(hole_cards):
                         return int(game_state['big_blind'])*10
