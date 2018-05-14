7ca50699bc30419d51b042fb100a45a43d6cdd12
Balint Bakcsa <balintbakcsa@gmail.com>
2018-05-14 12:09:49 +0200

    asdasd

--- player.py
+++ player.py
@@ -3,7 +3,7 @@ import card
 
 def get_own_player(players):
     for player in players:
-        if player.name == "Game On":
+        if player['name'] == 'Game On':
             print player
             return player
 
@@ -16,10 +16,9 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        print "GAME_STATE:" + str(game_state[u'players'])
-        print "GAME_STATE2:" + str(game_state['players'])
-        own_player = get_own_player(game_state["players"])
-        hole_cards = own_player["hole_cards"]
+        own_player = get_own_player(game_state['players'])
+        hole_cards = own_player['hole_cards']
+        print own_player
 
         ## if card.are_card_ranks_equal(hole_cards) or card.is_card_with_rank(hole_cards, "A"):
         #   return get_all_in_amount(own_player)
