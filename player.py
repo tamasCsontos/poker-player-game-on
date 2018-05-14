7b5ffd4bc6005fa650e46f17b0cdafbac7543b2f
Bende Hajdu <outdoorsport85@gmail.com>
2018-05-14 12:01:15 +0200

    fix

--- player.py
+++ player.py
@@ -16,14 +16,14 @@ class Player:
     VERSION = "Default Python folding player"
 
     def betRequest(self, game_state):
-        print game_state
-        own_player = get_own_player(game_state["players"])
-        hole_cards = own_player["hole_cards"]
+        # print game_state
+        # own_player = get_own_player(game_state["players"])
+        # hole_cards = own_player["hole_cards"]
+        #
+        # if card.are_card_ranks_equal(hole_cards) or card.is_card_with_rank(hole_cards, "A"):
+        #     return get_all_in_amount(own_player)
 
-        if card.are_card_ranks_equal(hole_cards) or card.is_card_with_rank(hole_cards, "A"):
-            return get_all_in_amount(own_player)
-
-        return 0
+        return 1000
 
     def showdown(self, game_state):
         pass
