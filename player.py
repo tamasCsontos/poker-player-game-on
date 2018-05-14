28bbf451ea9e54eb671b3526340e46617204474b
Balogh David <balogh.david1015@gmail.com>
2018-05-14 15:39:44 +0200

    1.01b

--- player.py
+++ player.py
@@ -24,12 +24,12 @@ def get_all_in_amount(player):
 
 
 class Player:
-    VERSION = "1.0b"
+    VERSION = "1.01b"
 
     def betRequest(self, game_state):
         own_player = get_own_player(game_state['players'])
         hole_cards = own_player['hole_cards']
-        if int(game_state['round']) == 1:
+        if int(game_state['pot']) <= game_state['big_blind'] * 4:
             if card.are_card_ranks_equal(hole_cards):
                     if card.is_card_under_ten(hole_cards):
                         return int(game_state['big_blind'])*10
@@ -40,7 +40,7 @@ class Player:
                     return int(game_state['big_blind'])*10
                 else:
                     return int(game_state['big_blind'])*2
-        elif int(game_state['round']) != 1:
+        elif int(game_state['pot']) > game_state['big_blind'] * 4:
             if card.are_card_ranks_equal(hole_cards):
                     if card.is_card_under_ten(hole_cards):
                         return int(game_state['current_buy_in']) * 2
